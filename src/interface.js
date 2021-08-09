daaadd749ca4094ad0dc14b222368b245bdf007d
William Young <wadyoung@outlook.com>
2021-08-09 15:29:04 +0100

    resolved conflicts

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-document.getElementById('note-body').style.height("690px");
-=======
 document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('note-body').style.height("690px");
 })
->>>>>>> a15d24f3022c676bf435d751ab6e7afb60a0306d
