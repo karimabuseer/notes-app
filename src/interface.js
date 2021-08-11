a9a59d71f32bbdec8bb9c873663b9a715ccc134e
William Young <wadyoung@outlook.com>
2021-08-11 15:18:51 +0100

    refactored index/added DOM elements

--- src/interface.js
+++ src/interface.js
@@ -1,2 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let button = document.getElementById("createNote")
+  let list = document.getElementById("notesList")
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault()
+    
+  })
 })
