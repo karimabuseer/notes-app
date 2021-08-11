99e3280baa7f27eae5666547eb775d78c52962bf
Sibel Eren <edaeren575@gmail.com>
2021-08-11 15:01:05 +0100

    display notes

--- src/interface.js
+++ src/interface.js
@@ -1,2 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // const storeNotes = new StoreNotes();
+  // const displayNotes = new DisplayNotes();
+  //
+  //
 })
--- src/storeNotes.js
+++ src/storeNotes.js
@@ -3,6 +3,8 @@ class StoreNotes {
   createNote = (value) => {
     let key = Date.now();
     localStorage.setItem(key,value);
+    //
+    // list_of_notes.push(key);
   };
 
 
