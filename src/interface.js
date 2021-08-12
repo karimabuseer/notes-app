cd0aaffff68674ff4fa580c9c2f7e8a5fecca3e6
karimabuseer <karimabuseer@gmail.com>
2021-08-12 17:14:30 +0100

    Working functions that add new notes and load existing notes from current storage

--- src/interface.js
+++ src/interface.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let list = document.getElementById("notesList");
   const displayNotes = new DisplayNotes();
   const storeNotes = new StoreNotes();
+  updateDisplay();
 
 
   button.addEventListener("click", (event) => {
@@ -10,11 +11,28 @@ document.addEventListener("DOMContentLoaded", () => {
     let allNotes = displayNotes.displayAll();
     let userInput = document.getElementById("writing").value;
     storeNotes.createNote(userInput);
+    newNote(userInput);
+  });
+
+  function updateDisplay () {
+    console.log(localStorage)
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      const newDiv = document.createElement("div");
+      const dummyText = document.createTextNode(value);
+      newDiv.appendChild(dummyText);
+      newDiv.classList.add("note");
+      list.appendChild(newDiv);
+    }
+  }
+
+  function newNote (userInput) { 
     const newDiv = document.createElement("div");
-    const dummyText = document.createTextNode(text);
+    const dummyText = document.createTextNode(userInput);
     newDiv.appendChild(dummyText);
     newDiv.classList.add("note");
     list.appendChild(newDiv);
-  });
+  }
   
 });
