4effa01dbfc97c9ed54bad5f3b71d943cb4ee241
Sibel Eren <edaeren575@gmail.com>
2021-08-13 11:06:49 +0100

    delete note

--- src/storeNotes.js
+++ src/storeNotes.js
@@ -3,11 +3,12 @@ class StoreNotes {
   createNote = (value) => {
     let key = Date.now();
     localStorage.setItem(key,value);
-    //
-    // list_of_notes.push(key);
   };
 
 
-
+  deleteNote = () => {
+      let key = location.hash.split("#")[1];
+      localStorage.removeItem(key);
+    };
 
 }
